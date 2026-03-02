e892f89a34fed93382da1d5dcd58e8f5a5c398dd
Long Đoàn <philongbettery@gmail.com>
2026-03-02 15:52:36 +0700

    Update vite.config.ts
    Signed-off-by: Long Đoàn <philongbettery@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -162,7 +162,7 @@ export default defineConfig({
     },
   },
   envDir: path.resolve(import.meta.dirname),
-  base: '/batteryphilong/',
+  base: '/batteryphilong/', 
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
